91bff093ec3b6d0f3d7df7c98441c9ffe78e1ec6
Zhent <jdcaballeror@correo.udistrital.edu.co>
2022-07-26 22:40:48 -0500

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class=""></div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Home",
+};
+</script>
+
+<style></style>
